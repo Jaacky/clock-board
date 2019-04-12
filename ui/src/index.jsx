7a915776e143d054afa956f58b4d31a679003d31
Jacky <jaackyvan@gmail.com>
2019-04-11 23:55:13 -0700

    Displaying end times of clocks

--- ui/src/index.jsx
+++ ui/src/index.jsx
@@ -2,10 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            clocks: [],
+            isLoading: false,
+        };
+    }
+
+    componentDidMount() {
+        this.setState({ isLoading: true });
+        fetch("/api/1")
+            .then((response) => {
+                return response.json();
+            })
+            .then((json) => {
+                this.setState({clocks: json.clocks, isLoading: false});
+                console.log(JSON.stringify(json));
+            });
+    }
     render() {
-        console.log("Hi?");
+        const { clocks, isLoading } = this.state;
+        if (isLoading) {
+            return <p>Loading...</p>
+        }
+
         return (
-            <div>Hello world from React</div>
+            <div>
+                {clocks.map(clock => {
+                    return <h2>{clock.end}</h2>
+                }
+                )}
+            </div>
         )
     }
 }
