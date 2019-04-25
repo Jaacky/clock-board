5889839f5765e47dcf2f5aed332032e9de70f738
Jacky <jaackyvan@gmail.com>
2019-04-25 00:46:41 -0700

    Refactoring out account related sagas

---
+++ ui/src/sagas/account.js
@@ -0,0 +1,124 @@
+import { call, put, takeEvery } from 'redux-saga/effects';
+
+import {
+    LOGIN_REQUEST,
+    REGISTRATION_REQUEST,
+    VERIFICATION_REQUEST,
+} from 'actions/types';
+
+import {
+    loginSuccessful,
+    loginFailed,
+    registrationSucceeded,
+    registrationFailed,
+    verficiationNeeded,
+    verificationSucceeded,
+    verificationFailed,
+} from 'actions';
+
+function* registration({email, password}) {
+    console.log("registration saga! emial:pass", email, password)
+    try {
+        const response = yield call(fetch, "/api/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email,
+                password,
+            }),
+        });
+        console.log("Response yielded: ", response);
+        console.log("response ok: ", response.ok);
+        const json = yield response.json();
+            console.log("json response from submit", json);
+        if (response.ok){
+            yield put(registrationSucceeded(json.email));
+            history.push("/verification");
+        } else {
+            yield put(registrationFailed(json.error));
+        }
+    } catch(err) {
+        console.log("Err in register saga", err);
+    }
+}
+
+export function* watchRegistration() {
+    yield takeEvery(REGISTRATION_REQUEST, registration)
+}
+
+function* verification({email, code}) {
+    try {
+        const response = yield call(fetch, "/api/verify", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email,
+                verificationCode: code,
+            }),
+        });
+        console.log("Response yielded: ", response);
+        console.log("response ok: ", response.ok);
+        const json = yield response.json();
+            console.log("json response from verification submit", json);
+        if (response.ok){
+            yield put(verificationSucceeded());
+            history.push("/dashboard");
+        } else {
+            yield put(verificationFailed(json.error));
+        }
+    } catch(err) {
+        console.log("Err in verification saga", err);
+    }
+}
+
+export function* watchVerification() {
+    yield takeEvery(VERIFICATION_REQUEST, verification);
+}
+
+function* login({email, password, history}) {
+    console.log("login saga, action: ", email, password);
+    // NO INTERNET TEST
+    // yield put(loginSuccessful({idToken: "123"}));
+    // return
+    // END NO INTERNET TEST
+    try {
+        const response = yield call(fetch, "/api/signin", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                "email": email,
+                "password": password,
+            }),
+        });
+        console.log("Response yielded: ", response);
+        console.log("response ok: ", response.ok);
+        const json = yield response.json();
+        console.log("json response from submit", json);
+        if (response.ok) {
+            if (json.newUser) {
+                console.log("Login, verification needed");
+                yield put(verficiationNeeded(email));
+                history.push("/verification");
+                return;
+            } else {
+                yield put(loginSuccessful({ idToken: json.idToken }));
+                history.push("/dashboard ");
+                return;
+            }
+        }
+    } catch(err) {
+        console.log("Err in login saga", err);
+    }
+
+    yield put(loginFailed("Error logging in. Please try again."));
+}
+
+export function* watchLogin() {
+    yield takeEvery(LOGIN_REQUEST, login)
+}
