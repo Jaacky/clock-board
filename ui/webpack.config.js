45e548ada948a7ef478cf577687f8b3463bb3d56
Jacky <jaackyvan@gmail.com>
2019-05-08 20:26:51 -0700

    Adding file-loader to handle font files

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -55,6 +55,16 @@ module.exports = {
                     },
                     "sass-loader",
                 ]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg|otf)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/'
+                    }
+                }]
             }
         ],
     },
